2f8dda8f7e5127a5989fc7529a03d67dbe8049c0
LoreleiGab <loreleigab@hotmail.com>
2016-09-14 13:29:58 -0300

    Módulo Gestão de Prazos

--- perfil/m_gestao_eventos/frm_busca.php
+++ perfil/m_gestao_eventos/frm_busca.php
@@ -20,9 +20,6 @@ if(isset($_POST['pesquisar'])){
 $id = trim($_POST['id']);
 $evento = trim($_POST['evento']);
 $fiscal = $_POST['fiscal'];
-$juridico = $_POST['juridico'];
-$processo = $_POST['NumeroProcesso'];
-$projeto = $_POST['projeto'];
 
 if($id == "" AND $evento == "" AND $fiscal == 0 AND $juridico == 0 AND $projeto == 0){ ?>
 <section id="services" class="home-section bg-white">
@@ -56,20 +53,6 @@ if($id == "" AND $evento == "" AND $fiscal == 0 AND $juridico == 0 AND $projeto 
 						<option value="0"></option>	
 						<?php echo opcaoUsuario($_SESSION['idInstituicao'],"") ?>
 						</select>
-                    <br />	
-                    
-					<label>Tipo de Relação Jurídica</label>
-						<select class="form-control" name="juridico" id="inputSubject" >
-						<option value='0'></option>
-						<?php  geraOpcaoOrdem("ig_modalidade","modalidade"); ?>
-						</select>							
-					<br/>
-					
-					<label>Tipo de Projeto</label>
-						<select class="form-control" name="projeto" id="inputSubject" >
-						<option value='0'></option>
-						<?php  geraOpcaoOrdem("ig_projeto_especial","projetoEspecial"); ?>
-                    </select>
             	</div>
             </div>
 				<br />             
@@ -92,7 +75,6 @@ if($id != "" AND $num_registro > 0){ // Foi inserido o número do pedido
 	$pedido = recuperaDados("igsis_pedido_contratacao",$id,"idEvento");
 	if($pedido['estado'] != NULL){
 	$evento = recuperaDados("ig_evento",$pedido['idEvento'],"idEvento"); //$tabela,$idEvento,$campo
-	$projeto = recuperaDados("ig_projeto_especial",$pedido['idEvento'],"projetoEspecial");
 	$usuario = recuperaDados("ig_usuario",$evento['idUsuario'],"idUsuario");
 	$instituicao = recuperaDados("ig_instituicao",$evento['idInstituicao'],"idInstituicao");
 	$local = listaLocais($pedido['idEvento']);
@@ -138,58 +120,24 @@ if($id != "" AND $num_registro > 0){ // Foi inserido o número do pedido
 		$filtro_fiscal = "";	
 	}
 		
-	if($juridico == 0){
-		$filtro_juridico = " ";	
-	}else{
-		$filtro_juridico = " AND ig_evento.ig_modalidade_IdModalidade = '$juridico'  ";	
-	}
-	
-	if($projeto == 0){
-		$filtro_projeto = " ";	
-	}else{
-		$filtro_projeto = " AND ig_evento.projetoEspecial = '$projeto'  ";	
-	}
-		
-	$sql_evento = "SELECT * FROM ig_evento,igsis_pedido_contratacao WHERE ig_evento.publicado = '1' AND igsis_pedido_contratacao.publicado = '1' AND ig_evento.idEvento = igsis_pedido_contratacao.idEvento $filtro_evento $filtro_fiscal $filtro_juridico $filtro_projeto AND estado IS NOT NULL ORDER BY ig_evento.idEvento DESC";
+	$sql_evento = "SELECT * FROM ig_evento WHERE ig_evento.publicado = '1' AND dataEnvio IS NULL AND ig_evento.idEvento NOT IN (SELECT DISTINCT idEvento FROM igsis_pedido_contratacao WHERE publicado = 1) $filtro_evento $filtro_fiscal ORDER BY ig_evento.idEvento DESC";
 	$query_evento = mysqli_query($con,$sql_evento);
 	$i = 0;
 	while($evento = mysqli_fetch_array($query_evento)){
 		$idEvento = $evento['idEvento'];	
-		$sql_existe = "SELECT idPedidoContratacao,idEvento FROM igsis_pedido_contratacao WHERE idEvento = '$idEvento' AND publicado = '1'";
-		$query_existe = mysqli_query($con, $sql_existe);
-		if(mysqli_num_rows($query_existe) > 0)
-			{
-			while($ped = mysqli_fetch_array($query_existe)){	
-			$pedido = recuperaDados("igsis_pedido_contratacao",$ped['idPedidoContratacao'],"idPedidoContratacao");
-			$evento = recuperaDados("ig_evento",$pedido['idEvento'],"idEvento"); //$tabela,$idEvento,$campo
-			$projeto = recuperaDados("ig_projeto_especial",$pedido['idEvento'],"projetoEspecial");
+			$evento = recuperaDados("ig_evento",$idEvento,"idEvento"); 			
 			$usuario = recuperaDados("ig_usuario",$evento['idUsuario'],"idUsuario");
-			$local = listaLocais($pedido['idEvento']);
-			$periodo = retornaPeriodo($pedido['idEvento']);
-			$pessoa = recuperaPessoa($pedido['idPessoa'],$pedido['tipoPessoa']);
-			$fiscal = recuperaUsuario($evento['idResponsavel']);
-			
-				if($pedido['publicado'] == 1){		
-				$x[$i]['id']= $evento['idEvento'];
-				$x[$i]['id_ped']= $pedido['idPedidoContratacao'];
-				$x[$i]['objeto'] = retornaTipo($evento['ig_tipo_evento_idTipoEvento'])." - ".$evento['nomeEvento'];
+			$local = listaLocais($idEvento);
+			$periodo = retornaPeriodo($idEvento);
+			$fiscal = recuperaUsuario($evento['idResponsavel']);			
 				
-				if($pedido['tipoPessoa'] == 1){
-				$pessoa = recuperaDados("sis_pessoa_fisica",$pedido['idPessoa'],"Id_PessoaFisica");
-				$x[$i]['proponente'] = $pessoa['Nome'];
-				$x[$i]['tipo'] = "Física";
-				}else{
-				$pessoa = recuperaDados("sis_pessoa_juridica",$pedido['idPessoa'],"Id_PessoaJuridica");
-				$x[$i]['proponente'] = $pessoa['RazaoSocial'];
-				$x[$i]['tipo'] = "Jurídica";
-				}
-			$x[$i]['local'] = substr($local,1);
-			$x[$i]['periodo'] = $periodo;
-			$x[$i]['fiscal'] = $fiscal['nomeCompleto'];			
-			$i++;
-				}
-			}
-			}
+				$x[$i]['id']= $evento['idEvento'];
+				$x[$i]['objeto'] = retornaTipo($evento['ig_tipo_evento_idTipoEvento'])." - ".$evento['nomeEvento'];
+				$x[$i]['local'] = substr($local,1);
+				$x[$i]['periodo'] = $periodo;
+				$x[$i]['fiscal'] = $fiscal['nomeCompleto'];			
+				$i++;			
+			
 	}
 	$x['num'] = $i;		
 }
@@ -213,8 +161,6 @@ $mensagem = "Foram encontradas ".$x['num']." pedido(s) de contratação.";
 					<thead>
 						<tr class="list_menu">
 							<td>Id Evento</td>
-							<td>Codigo do Pedido</td>
-							<td>Proponente</td>
 							<td>Objeto</td>
 							<td>Local</td>
 							<td>Periodo</td>
@@ -229,9 +175,7 @@ $data=date('Y');
 for($h = 0; $h < $x['num']; $h++)
  {
 		
-	echo "<tr><td class='list_description'> <a href='".$link.$x[$h]['id']."'>".$x[$h]['id']."</a></td>";
-	echo '<td class="list_description">'.$x[$h]['id_ped'].		'</td>';
-	echo '<td class="list_description">'.$x[$h]['proponente'].			'</td> ';
+	echo "<tr><td class='list_description'> <a href='".$link.$x[$h]['id']."'>".$x[$h]['id']."</a></td>";	
 	echo '<td class="list_description">'.$x[$h]['objeto'].				'</td> ';
 	echo '<td class="list_description">'.$x[$h]['local'].				'</td> ';
 	echo '<td class="list_description">'.$x[$h]['periodo'].				'</td> ';
@@ -281,20 +225,6 @@ for($h = 0; $h < $x['num']; $h++)
 						<option value="0"></option>	
 						<?php echo opcaoUsuario($_SESSION['idInstituicao'],"") ?>
 						</select>
-                    <br />
-                    	                   
-					<label>Tipo de Relação Jurídica</label>
-						<select class="form-control" name="juridico" id="inputSubject" >
-						<option value='0'></option>
-						<?php  geraOpcaoOrdem("ig_modalidade","modalidade"); ?>
-						</select>							
-					<br/>
-					
-					<label>Tipo de Projeto</label>
-						<select class="form-control" name="projeto" id="inputSubject" >
-						<option value='0'></option>
-						<?php  geraOpcaoOrdem("ig_projeto_especial","projetoEspecial"); ?>
-                    </select>	
 
             	</div>
              </div>
